fc6ae25fa86badf1ea12659b71cbaf1f399da4f2
Chrystian Moté Barbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-08-31 19:59:37 -0300

    feat: Exercício 1 v2 explicado

--- exercicio_01v2.js
+++ exercicio_01v2.js
@@ -7,8 +7,15 @@ for (let i = 0; i < 5; i += 1) {
     numerostr = prompt("Informe um número:");
     num.push(numerostr);
 }
+
 for (let i = 0; i < num.length; i++) {
-    if((num.length - 1) == i)
+
+    // O total de Elementos - 1 equivale a ultima posição do vetor.
+    // Ex: 5 valores no vetor, vai de 0 a 4 posições [0,1,2,3,4]. 
+    // O objetivo é inserir o ponto final no ultimo valor no texto.
+    let ultimaPosicao = (num.length - 1); 
+    
+    if(i == ultimaPosicao)
         mensagem += num[i] + ".";
     else
         mensagem += num[i] + ", ";    
