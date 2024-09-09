a1d0c7b6378f0e24213f2873e46414bcfc552791
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-09-09 20:30:57 -0300

    feat: Exercício 4 concluído!

---
+++ exercicio_04.js
@@ -0,0 +1,33 @@
+/*Crie um algoritmo que leia o salário bruto de 5 pessoas, 
+e ao final, mostre o salário líquido de todos, 
+considerando 12% de INSS a descontar.
+*/
+let listaSalarios = [];
+for (let i = 0; i < 5; i++) {
+    //leitura do salário em String
+    let salarioString = prompt("Informe o Salario: ");
+    //conversão do salário de string para float (número decimal)
+    let salario = parseFloat(salarioString);
+    //insere na lista (vetor)
+    listaSalarios.push(salario);
+}
+
+let mensagem = "";
+for (let j = 0; j < listaSalarios.length; j++) {
+
+    // F1: 
+    // desconto = salariobruto x 12/100 ;
+    // salarioliquido = salariobruto - desconto 
+
+    // F2: 100-12 = 88 
+    // salarioliquido =  salariobruto X 88/100;
+
+    // F1: let salLiquido =  listaSalarios[j] - (listaSalarios[j] * (12/100)); 
+    let salLiquido = listaSalarios[j] * (88 / 100);
+    mensagem += "Salarios Líquidos: \n";
+    mensagem += "R$ " + salLiquido.toFixed(2) + "\n";
+}
+alert(mensagem);
+
+
+
